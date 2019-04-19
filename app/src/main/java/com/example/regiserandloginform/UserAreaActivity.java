3d84a57f46d8630d587b9bf4a870fa6d1a6c7037
rezsutab2 <rezsuta.b2@gmail.com>
2019-04-19 18:03:38 +0200

    User Area Activity-ből NavigationActivity-be való átjelentkezés

--- app/src/main/java/com/example/regiserandloginform/UserAreaActivity.java
+++ app/src/main/java/com/example/regiserandloginform/UserAreaActivity.java
@@ -3,6 +3,8 @@ package com.example.regiserandloginform;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -13,9 +15,10 @@ public class UserAreaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
 
-        final EditText etUsername=findViewById(R.id.etUsername);
-        final EditText etAge=findViewById(R.id.etAge);
+        final TextView etUsername=findViewById(R.id.etUsername);
+        final TextView etAge=findViewById(R.id.etAge);
         final TextView tvWelcomeMsg=findViewById(R.id.tvWelcomeMsg);
+        final Button bNext=findViewById(R.id.bNext);
 
         Intent intent=getIntent();
         String name=intent.getStringExtra("name");
@@ -26,5 +29,15 @@ public class UserAreaActivity extends AppCompatActivity {
         tvWelcomeMsg.setText(message);
         etUsername.setText(username);
         etAge.setText(String.valueOf(age));
+
+        bNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(UserAreaActivity.this,
+                        NavigationActivity.class);
+                UserAreaActivity.this.startActivity(intent);
+
+            }
+        });
     }
 }
